4965a0c6b3e1a8ff6f587969f79d114875198459
morpy <morpy@morpys-MacBook-Pro.local>
2020-03-31 18:50:26 +0700

    Updated

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,14 +1,170 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Animated, StyleSheet, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const HEADER_HEIGHT = 60;
+const MAX_SCROLL_OFFSET = 400;
 
 class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scrollY: new Animated.Value(0)
+    }
+  }
   render () {
+    const translateDiffScroll = Animated.diffClamp(this.state.scrollY, 0, MAX_SCROLL_OFFSET)
+      .interpolate({
+        inputRange:[0, MAX_SCROLL_OFFSET],
+        outputRange: [0, -HEADER_HEIGHT],
+        extrapolate: 'clamp'
+      })
     return (
-      <View style={{flex:1, justifyContent:'center',}}>
-        <Text style={{color:'#ff00ff'}}>Home</Text>
-      </View>
+      <Animated.View style={styles.root}>
+        <Animated.View style={[styles.headerTop, {transform: [{translateY: translateDiffScroll}]}]}>
+          <LinearGradient colors={['#FAFAF8', '#EEFFBA']} style={{ flex: 1 }} style={styles.head}>
+            <View style={styles.left}>
+              <Image source={require('../../assets/player.png')} style={styles.img1}/>
+              <Image source={require('../../assets/leaf.png')} style={styles.img2} />
+            </View>
+            <View style={styles.right}>
+              <Text style={{ color:'#46919F', fontSize:44, position:'absolute', marginTop:100, fontWeight:'bold'}}>
+                Welcome
+              </Text>
+              <Text style={{ color: '#46919F', fontSize: 18, position: 'absolute', marginTop: 150, right:30, }}>
+                XYZ Team App
+              </Text>
+              <Image source={require('../../assets/ground.png')} style={styles.img3} />
+            </View> 
+          </LinearGradient>
+          <View style={styles.buttonTab}>
+            <TouchableOpacity style={styles.tab} onPress={() => this.props.navigation.navigate('Home')}>
+              <Text style={styles.buttonText}>About</Text>
+              <View style={{ width: '100%', height: 3, borderRadius: 10, backgroundColor: '#7BB2D4', marginTop: 10 }} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.tab} onPress={() => this.props.navigation.navigate('Achievment')}>
+              <Text style={styles.buttonText}>Achievment</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.tab} onPress={() => this.props.navigation.navigate('Training')}>
+              <Text style={styles.buttonText}>Training</Text>
+            </TouchableOpacity>
+          </View>
+        </Animated.View>
+        <Animated.ScrollView style={styles.body}>
+          <View style={styles.section}>
+            <Text style={{ color:'#7BB2D4', fontSize:35, fontWeight:'bold', marginVertical:10}}>
+              Why XYZ ?
+            </Text>
+            <Text style={styles.content}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore 
+              magna aliqua. Ut enim ad minim veniam, quis nostrud 
+              exercitation ullamco laboris nisi ut aliquip ex ea 
+              commodo consequat.
+            </Text>
+            <Text style={styles.content}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </Text>
+            <Text style={styles.content}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </Text>
+            <Text style={styles.content}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </Text>
+          </View>
+        </Animated.ScrollView>
+      </Animated.View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  root:{
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  headerTop:{
+    flex: 1
+  },
+  head:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'flex-end',
+    flex:1,
+    zIndex:-50
+  },
+  body:{
+    flex:1,
+    paddingHorizontal:30,
+  },
+  left:{
+    flex:1,
+    top:-300
+  },
+  right: {
+    flex: 1,
+    top:-300
+  },
+  img1:{
+    width:173,
+    height:211,
+    position:'absolute',
+    bottom:-300,
+    zIndex:10,
+  },
+  img2: {
+    width: 224.75,
+    height: 178.5,
+    position: 'absolute',
+    bottom: -300,
+    left:-30,
+  },
+  img3: {
+    width: 341.86,
+    height: 64,
+    position: 'absolute',
+    bottom: -300,
+    right:-10,
+  },
+  buttonTab: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 20,
+    top: -40,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 40,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  tab:{
+    padding:10,
+  },
+  buttonText:{
+    fontSize:18,
+    color:'#7BB2D4'
+  },
+  content:{
+    lineHeight:20,
+    fontSize:12
+  },
+  section:{
+    paddingBottom:10,
+  }
+})
+
 export default Index;
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators, } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../components/home';
+import AchievmentScreen from '../components/home/achievment';
+import TrainingScreen from '../components/home/training';
+
 import OurTeamScreen from '../components/ourteam';
 import TeamInfoScreen from '../components/teaminfo';
 
-BottomTab = createBottomTabNavigator();
 
+HomeStack = createStackNavigator();
+const HomeStackNavigator = ({navigation}) => (
+  <HomeStack.Navigator
+    initialRouteName="Home"
+  >
+    <HomeStack.Screen name="Home"
+      component={HomeScreen}
+      options={{
+        headerTitle:null,
+        headerLeft:null,
+        headerTransparent:true,
+        cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+      }}
+    />
+    <HomeStack.Screen name="Achievment"
+      component={AchievmentScreen}
+      options={{
+        headerTitle: null,
+        headerLeft: null,
+        headerTransparent: true,
+      }}
+    />
+    <HomeStack.Screen name="Training"
+      component={TrainingScreen}
+      options={{
+        headerTitle: null,
+        headerLeft: null,
+        headerTransparent: true,
+      }}
+    />
+  </HomeStack.Navigator>
+)
+
+BottomTab = createBottomTabNavigator();
 const Tab = ({navigation}) => (
   <BottomTab.Navigator initialRouteName="Home"
     screenOptions={({ route }) => ({
@@ -43,10 +79,15 @@ const Tab = ({navigation}) => (
     tabBarOptions={{
       activeTintColor: '#7BB2D4',
       inactiveTintColor: '#CCCCCC',
+      style:{
+        height:75,
+        paddingBottom:10,
+        paddingTop: 10,
+        elevation:0,
+      }
     }}
-    barStyle={{backgrounColor:'#ff0000'}}
   >
-    <BottomTab.Screen name="Home" component={HomeScreen} />
+    <BottomTab.Screen name="Home" component={HomeStackNavigator} />
     <BottomTab.Screen name="OurTeam" component={OurTeamScreen} />
     <BottomTab.Screen name="TeamInfo" component={TeamInfoScreen} />
   </BottomTab.Navigator>
